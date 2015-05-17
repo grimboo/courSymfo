032ce3c1e74bba7eab560cdb76e71c30984ec0a3
grimboo <boin.jeremy.pro@gmail.com>
2015-05-17 17:45:20 +0200

    Creation du controller 'VitrineController'

---
+++ src/Binss/VitrineBundle/Controller/VitrineController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Binss\VitrineBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+/*class VitrineController extends Controller
+{
+   public function indexAction($name)
+    {
+        return $this->render('BinssVitrineBundle:Default:index.html.twig', array('name' => $name));
+    }
+}*/
